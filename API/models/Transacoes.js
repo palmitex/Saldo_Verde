9a3160966c2111043b5372b369b99980b1208d63
palmitex <gabriel1208.palmieri@gmail.com>
2025-05-15 17:20:52 -0300

    Arrumando o transação models

--- API/models/Transacoes.js
+++ API/models/Transacoes.js
@@ -1,11 +1,21 @@
 import {  update, create } from "../config/database.js";
 
 const criarTransacao = async (dados) => {
+   try{
     return await create('transacoes', dados);
+   } catch(err){
+    console.error('Erro ao criar uma transação: ', err)
+    throw err;
+   }
 };
 
 const atualizarTransacao = async (id, dados) => {
-    return await update('transacoes', dados, `id = ${id}`);
+    try{
+        return await update('transacoes', dados, `id = ${id}`);
+    } catch(err){
+        console.error('Erro ao atualizar uma transação: ', err)
+        throw err;
+    }
 };
 
 export {
